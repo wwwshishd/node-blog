325ed6290598941569c70a5e0b1490f72d593c83
wwwshishd <wwwshishd@gmail.com>
2014-09-09 13:37:45 +0800

    upload  ...not unload..  I have no idea.

--- requestHandlers.js
+++ requestHandlers.js
@@ -6,7 +6,7 @@ exports.start = function(response) {
 	response.end();
 }
 
-exports.unload = function(response) {
+exports.upload = function(response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.write('hello upload.');
 	response.end();
